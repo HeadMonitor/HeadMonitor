9b86e3cd626207fff1d35ed74096370b0803fcb5
Hector Angelopoulos <hectorangelopoulos@gmail.com>
2024-11-11 19:49:06 +0200

    GitHub Pages integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Footer from './Elements/Footer';
 import Projects from './Elements/Projects';
+import Background from './Images/Background.png'
+import HeadMonitor from './Images/HeadMonitor.png'
 
 function App() 
 {
   return (
-    <div className="App bg-black">
-      <div className='flex flex-col min-h-screen justify-center items-center'>
+    <div className="App" style={{ backgroundImage: `url(${Background})` }}>
+      <title>HeadMonitor</title>
+
+      <div className='flex flex-col min-h-screen justify-between items-center'>
+        <br/>
+        <img src={HeadMonitor} alt='HeadMonitor' width='256' className='outline outline-[0.5rem]'/>
         <h1 className='text-white font-[PixelatedFont] drop-shadow-[0_1.2px_5px_rgba(255,0,132,0.8)]'>
           HeadMonitor
         </h1>
--- src/Elements/Projects.js
+++ src/Elements/Projects.js
@@ -1,13 +1,15 @@
 import '../App.css';
+import ScreenCrowStudiosLogo from '../Images/ScreenCrow Studios Logo.png'
 
 function Projects() 
 {
   return (
     <div>
       <div className='text-white'>
-        <h2 className='font-[PixelatedFont] drop-shadow-[0_1.2px_5px_rgba(255,0,132,0.8)]'>
+        <h2 className='font-[PixelatedFont] drop-shadow-[0_1.2px_5px_rgba(255,0,132,0.8)] underline'>
             My Projects
         </h2>
+        <img src={ScreenCrowStudiosLogo} width='256'/>
       </div>
     </div>
   );
